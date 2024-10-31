09e7deb5fe502a506fd8b122c7ce11658a903e68
Yukimi Miyahara <yukimi_pooh@au.com>
2024-10-31 12:27:06 +0900

    TesT plus

--- test.bash
+++ test.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2024 Yukimi Miyahara  # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,8 +7,10 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
